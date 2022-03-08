98f31e71736044ebbbf59132fe1a9bb018d76944
unknown <okorojiebube2@gmail.com>
2022-03-08 00:53:35 -0600

    Refactor user.js file

--- src/users.js
+++ src/users.js
@@ -1,31 +1,89 @@
 import './users.css'
 import renderNav from './dashboard/nav'
 import renderPage from './dashboard/dashboardHome'
-import userStories from './dashboard/usersStories'
-import favouriteStories from './dashboard/favouriteStory'
+import renderUserStories from './dashboard/usersStories'
+import renderfavouriteStories from './dashboard/favouriteStory'
+import renderPublished from './dashboard/publishedStories'
 
 renderNav()
 renderPage()
-userStories()
-favouriteStories()
+renderUserStories()
+renderfavouriteStories()
+renderPublished()
 
-document.querySelectorAll('.list').forEach(e => {
-	e.addEventListener('click', displayPage)
-})
-
-document.querySelectorAll('.view-stories').forEach(e => {
-	e.addEventListener('click', displayStory)
-})
-
-document.querySelector('#write').addEventListener('click', () => {
-	document.querySelector('.created-stories').style.display = 'none'
-	document.querySelector('.write-story').style.display = 'block'
-})
 
 const showDrafts = () => {
 	document.querySelector('.published').style.display='none'
 	document.querySelector('#published').style.display='flex'
 }
+
+showDrafts()
+
+const showStories = () => {
+	document.querySelectorAll('section').forEach(section => {
+		section.style.display = "none"
+	})
+	document.querySelector('.main').style.display='block'
+	document.querySelector('#main').style.color='white'
+}
+
+showStories()
+
+const navigatePages = () => {
+	document.querySelectorAll('.list').forEach(navList => {
+	navList.addEventListener('click', (e) => {
+	const list = e.target.id
+	const activeSection = document.querySelector(`.${list}`)
+	const navList = document.querySelectorAll('.list i')
+	const activeList = document.querySelector(`#${list}`)
+
+	document.querySelectorAll('section').forEach(section => {
+		section.style.display = 'none'
+		let { className } = section
+		if (className === list) {
+			navList.forEach(list => {
+				list.style.color = '#ce6a10'
+			})
+			activeList.style.color = 'white'
+		}
+	})
+	activeSection.style.display = 'block'
+	})
+})
+}
+
+navigatePages()
+
+const displayStory = () => {
+  document.querySelectorAll('.view-stories').forEach(btn => {
+	btn.addEventListener('click', (e) => {
+	const myStoryId = e.target.id
+	const activeSection = document.querySelector(`.${myStoryId}`)
+	const activeStoryBtn = document.querySelectorAll('.view-stories')
+	const activeStory = document.querySelector(`#${myStoryId}`)
+
+	document.querySelectorAll('.my-stories').forEach(story => {
+		story.style.display = 'none'
+		let { className } = story
+		className = className.split(' ')[0]
+		if (className === myStoryId) {
+			activeSection.style.display = 'flex'
+			activeStoryBtn.forEach(list => {
+				list.style.color = '#000'
+			})
+			activeStory.style.color = '#ce6a10'
+		}
+	})
+	})
+})
+}
+
+displayStory()
+
+document.querySelector('#write').addEventListener('click', () => {
+	document.querySelector('.created-stories').style.display = 'none'
+	document.querySelector('.write-story').style.display = 'block'
+})
 
 const togglePreview = () => {
 	const previewBtn = document.querySelector('#preview-btn')
@@ -45,58 +103,6 @@ const togglePreview = () => {
 
 togglePreview()
 
-function displayPage(e) {
-	const list = e.target.id
-	const activeSection = document.querySelector(`.${list}`)
-	const navList = document.querySelectorAll('.list i')
-	const activeList = document.querySelector(`#${list}`)
-
-	document.querySelectorAll('section').forEach(section => {
-		section.style.display = 'none'
-		let { className } = section
-		if (className === list) {
-			navList.forEach(list => {
-				list.style.color = '#ce6a10'
-			})
-			activeList.style.color = 'white'
-		}
-	})
-	activeSection.style.display = 'block'
-}
-
-const showStories = () => {
-	document.querySelectorAll('section').forEach(section => {
-		section.style.display = "none"
-	})
-	document.querySelector('.main').style.display='block'
-	document.querySelector('#main').style.color='white'
-}
-
-function displayStory(e) {
-	const myStoryId = e.target.id
-	const activeSection = document.querySelector(`.${myStoryId}`)
-	const activeStoryBtn = document.querySelectorAll('.view-stories')
-	const activeStory = document.querySelector(`#${myStoryId}`)
-
-	document.querySelectorAll('.my-stories').forEach(story => {
-		story.style.display = 'none'
-		let { className } = story
-		className = className.split(' ')[0]
-		if (className === myStoryId) {
-			activeSection.style.display = 'flex'
-			activeStoryBtn.forEach(list => {
-				list.style.color = '#000'
-			})
-			activeStory.style.color = '#ce6a10'
-		}
-	})
-}
-
-window.onload = () => {
-	showStories()
-		showDrafts()
-}
-
 const openModal = () => {
 	const modalButtons = Array.from(document.querySelectorAll('.popup'))
 	const modals = Array.from(document.querySelectorAll('.modal'))
@@ -108,6 +114,7 @@ const openModal = () => {
 		})
 	})
 }
+
 openModal()
 
 const closeModal = () => {
@@ -119,9 +126,11 @@ const closeModal = () => {
 		})
 	})
 }
+
 closeModal()
 
-document.querySelector('#file').addEventListener('input', function (e) {
+const renderImage = () => {
+  document.querySelector('#file').addEventListener('input', function (e) {
 	const textArea = document.querySelector('.text-area')
 	let image = document.querySelector('.text-img')
 
@@ -137,10 +146,17 @@ document.querySelector('#file').addEventListener('input', function (e) {
 		reader.readAsDataURL(file)
 	}
 })
-
-const displayText = e => {
-	document.querySelector('#text').innerHTML = e.target.innerHTML
 }
 
-const textArea = document.querySelector('.text-area')
-textArea.addEventListener('input', displayText)
+renderImage()
+
+const displayText = e => {
+	const textArea = document.querySelector('.text-area')
+textArea.addEventListener('input', (e) => {
+  document.querySelector('#text').innerHTML = e.target.innerHTML
+})
+}
+
+displayText()
+
+
