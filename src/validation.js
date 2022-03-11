027ec64c83af908ad39423934f9dc01a377ea201
unknown <okorojiebube2@gmail.com>
2022-03-10 20:24:59 -0600

    Modify email validation functionality

--- src/validation.js
+++ src/validation.js
@@ -1,28 +1,23 @@
-const displayMsg = (input, message, status) => {
-	const emailMsg = document.querySelector('#email-msg')
-	const passwordMsg = document.querySelector('#password-msg')
-
-	emailMsg.innerText = message
-	// passwordMsg.innerText = message
-
+const displayMsg = (input, message, status, element) => {
+	element.innerText = message
 	input.className = status ? 'success' : 'error'
 	return status
 }
 
-const showError = (input, message) => {
-	return displayMsg(input, message, false)
+const showError = (input, message, element) => {
+	return displayMsg(input, message, false, element)
 }
 
-const showSuccess = (input) => {
+const showSuccess = input => {
 	return displayMessage(input, '', true)
 }
 
-const validateEmail = (input, invalidMsg) => {
+const validateEmail = (input, invalidMsg, element) => {
 	const emailRegex = /^([a-z 0-9 \. -]+)@([a-z 0-9 -]+)\.([a-z 0-9 \. -]{2,5}).([a-z 0-9]{2,8})?$/
 
 	const email = input.value.trim()
 	if (!emailRegex.test(email) || email !== email.toLowerCase()) {
-		return showError(input, invalidMsg)
+		return showError(input, invalidMsg, element)
 	}
 	showSuccess(input)
 	return true
