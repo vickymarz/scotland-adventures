5bc483571bc1169912fd59cc1c8b11cfc97b4922
unknown <okorojiebube2@gmail.com>
2022-03-14 08:51:43 -0600

    Add forgot password functionality

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,11 +1,14 @@
-import { fetchApi } from "./fetchApi";
-import {BASE_URL, SIGNUP_URL, LOGIN_URL} from './rootEndpoints'
-	
+import { fetchApi } from './fetchApi'
+import { BASE_URL, SIGNUP_URL, LOGIN_URL, FORGOT_PASSWORD_URL } from './rootEndpoints'
 
-const register = async(params) => {
-  await fetchApi.post(`${BASE_URL}/${SIGNUP_URL}`, params)
+const register = async params => {
+	await fetchApi.post(`${BASE_URL}/${SIGNUP_URL}`, params)
 }
 
-const login = async(params) => {
-  await fetchApi.post(`${BASE_URL}/${LOGIN_URL}`, params)
+const login = async (email, password) => {
+	await fetchApi.post(`${BASE_URL}/${LOGIN_URL}`, { email, password })
+}
+
+const forgotPassword = async email => {
+	await fetchApi.post(`${BASE_URL}/${(LOGIN_URL, FORGOT_PASSWORD_URL)}`, { email })
 }
