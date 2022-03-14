7528c201ade76d773561d8b677bb2d1628734e83
unknown <okorojiebube2@gmail.com>
2022-03-14 08:45:20 -0600

    Add login functionality

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,6 +1,11 @@
-import { fetchApi } from './fetchApi'
-import { BASE_URL, SIGNUP_URL } from './rootEndpoints'
+import { fetchApi } from "./fetchApi";
+import {BASE_URL, SIGNUP_URL, LOGIN_URL} from './rootEndpoints'
+	
 
-const register = params => {
-	fetchApi.post(`${BASE_URL}/${SIGNUP_URL}`, params)
+const register = async(params) => {
+  await fetchApi.post(`${BASE_URL}/${SIGNUP_URL}`, params)
+}
+
+const login = async(params) => {
+  await fetchApi.post(`${BASE_URL}/${LOGIN_URL}`, params)
 }
