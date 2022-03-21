50d5dd15ae9beeb948adc0c2c41d383d10a9efaa
unknown <okorojiebube2@gmail.com>
2022-03-21 15:34:18 -0600

    Modify new admin function

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -10,6 +10,9 @@ import {
 
 const register = async params => {
 	const result = await fetchApi.post(`${BASE_URL}/${SIGNUP_URL}`, params)
+	const container = document.querySelector('.signup-success')
+	const success = document.querySelector('.success-text')
+
 	if (result.message === 'User with email already exists') {
 		const error = document.querySelector('.verify-email')
 		error.style.display = 'flex'
@@ -19,38 +22,59 @@ const register = async params => {
 		}, 3000)
 		return result
 	}
+	if (result.code === 200) {
+		container.style.display = 'block'
+		setTimeout(() => {
+			container.style.display = 'none'
+		}, 5000)
+	}
+
 	if (result.code === 204) {
-		const success = document.querySelector('.success-text')
+		container.style.display = 'block'
 		success.textContent =
 			'Your email has been verified. You already have an account. Kindly login with your admin email and password'
 		setTimeout(() => {
-			error.style.display = 'none'
-		}, 3000)
+			container.style.display = 'none'
+		}, 5000)
 	}
+
 	if (result.code === 208) {
-		const success = document.querySelector('.success-text')
+		container.style.display = 'block'
 		success.textContent = 'You are already a basic User!'
 		setTimeout(() => {
-			error.style.display = 'none'
-		}, 3000)
+			container.style.display = 'none'
+		}, 5000)
 	}
 }
+
 const adminRegister = async params => {
 	const result = await fetchApi.post(`${BASE_URL}/${ADMIN_SIGNUP_URL}`, params)
+	const success = document.querySelector('.success-text')
+	const container = document.querySelector('.signup-success')
+
+	if (result.code === 200) {
+		container.style.display = 'block'
+		setTimeout(() => {
+			container.style.display = 'none'
+		}, 5000)
+	}
+
 	if (result.code === 204) {
-		const success = document.querySelector('.success-text')
+		container.style.display = 'block'
 		success.textContent =
 			'Your email has been verified. You already have an account. Kindly login with your basic user email and password'
 		setTimeout(() => {
-			error.style.display = 'none'
-		}, 3000)
+			console.log(container)
+			container.style.display = 'none'
+		}, 5000)
 	}
+
 	if (result.code === 208) {
-		const success = document.querySelector('.success-text')
+		container.style.display = 'block'
 		success.textContent = 'You are already an admin!'
 		setTimeout(() => {
-			error.style.display = 'none'
-		}, 3000)
+			container.style.display = 'none'
+		}, 5000)
 	}
 	return result
 }
