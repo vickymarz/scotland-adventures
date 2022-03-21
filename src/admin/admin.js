1b2db02d01b2faf0d34fe0fd8bdf69dad1187c41
unknown <okorojiebube2@gmail.com>
2022-03-21 16:31:10 -0600

    Add admin login function

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,1 +1,17 @@
 import './admin.css'
+import { userServices } from '../services/userServices'
+
+const loginValidation = () => {
+  const form = document.querySelector('#login-form')
+
+	form.addEventListener('submit', e => {
+		e.preventDefault()
+		const email = form.elements.email.value
+		const password = form.elements.password.value
+		const currentUser = { email, password }
+		userServices.adminLogin(currentUser)
+		form.reset()
+	})
+}
+
+loginValidation()
