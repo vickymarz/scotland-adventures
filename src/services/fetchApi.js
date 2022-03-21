26a562725e891f46b9abc54053b7d4cc541d0981
unknown <okorojiebube2@gmail.com>
2022-03-21 14:15:02 -0600

    Modify fetch api

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -23,7 +23,6 @@ const post = async (url, data) => {
 	try {
 		const response = await fetch(url, config)
 		const datas = await response.json()
-		console.log(datas);
 		return datas
 	} catch (err) {
 		return err
@@ -63,7 +62,6 @@ const put = async (url, data) => {
 		return err
 	}
 }
-
 
 export const fetchApi = {
 	post,
